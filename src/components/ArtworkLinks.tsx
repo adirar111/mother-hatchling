471d28e4478540f069060ce337bf73e694e64381
aymen <63937200+adirar111@users.noreply.github.com>
2023-04-13 21:06:32 -0700

    tweaks

--- src/components/ArtworkLinks.tsx
+++ src/components/ArtworkLinks.tsx
@@ -4,7 +4,6 @@ export default function ArtworkLinks() {
   // TODO: add collaborative piece link when implemented
   return (
     <div>
-      <h2>artworks</h2>
       <ul style={{ listStyleType: "none" }}>
         {Artworks.map((artwork, index) => (
           <li key={index}>
